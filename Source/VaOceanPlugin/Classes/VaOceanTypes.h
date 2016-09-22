dafeb8bffbe7ecbcf8d1efb807638ec316daf720
v.alyamkin <ufna@ufna.ru>
2016-09-22 14:42:47 +0300

    Blueprint accessors fixed

--- Source/VaOceanPlugin/Classes/VaOceanTypes.h
+++ Source/VaOceanPlugin/Classes/VaOceanTypes.h
@@ -10,43 +10,43 @@ struct FSpectrumData
 {
 	GENERATED_USTRUCT_BODY()
 
-	/** The size of displacement map. Must be power of 2. */
-	UPROPERTY(BlueprintReadOnly, Category = Ocean)
+	/** The size of displacement map. Must be power of 2.
+	 * Not editable because of FFT shader config */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
 	int32 DispMapDimension;
 
 	/** The side length (world space) of square patch. Typical value is 1000 ~ 2000. */
-	UPROPERTY(EditDefaultsOnly, Category = Ocean)
+	UPROPERTY(EditAnywhere)
 	float PatchLength;
 
 	/** Adjust the time interval for simulation (controls the simulation speed) */
-	UPROPERTY(EditDefaultsOnly, Category = Ocean)
+	UPROPERTY(EditAnywhere)
 	float TimeScale;
 
 	/** Amplitude for transverse wave. Around 1.0 (not the world space height). */
-	UPROPERTY(EditDefaultsOnly, Category = Ocean)
+	UPROPERTY(EditAnywhere)
 	float WaveAmplitude;
 
 	/** Wind direction. Normalization not required */
-	UPROPERTY(EditDefaultsOnly, Category = Ocean)
+	UPROPERTY(EditAnywhere)
 	FVector2D WindDirection;
 
-	/** The bigger the wind speed, the larger scale of wave crest. But the wave scale can be no larger than PatchLength.
-	Around 100 ~ 1000 */
-	UPROPERTY(EditDefaultsOnly, Category = Ocean)
+	/** The bigger the wind speed, the larger scale of wave crest. But the wave scale can be no larger than PatchLength. Around 100 ~ 1000 */
+	UPROPERTY(EditAnywhere)
 	float WindSpeed;
 
 	/** This value damps out the waves against the wind direction. Smaller value means higher wind dependency. */
-	UPROPERTY(EditDefaultsOnly, Category = Ocean)
+	UPROPERTY(EditAnywhere)
 	float WindDependency;
 
 	/** The amplitude for longitudinal wave. Higher value creates pointy crests. Must be positive. */
-	UPROPERTY(EditDefaultsOnly, Category = Ocean)
+	UPROPERTY(EditAnywhere)
 	float ChoppyScale;
 
 	/** Defaults */
 	FSpectrumData()
 	{
-		DispMapDimension = 512;		// Not editable because of FFT shader config
+		DispMapDimension = 512;
 		PatchLength = 2000.0f;
 		TimeScale = 0.8f;
 		WaveAmplitude = 0.35f;
